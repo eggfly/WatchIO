61005de704070aebff88f02817cbb97bcf88fe79
eggfly <eggfly@qq.com>
2019-08-09 12:58:28 +0800

    add rtclib
    Signed-off-by: eggfly <eggfly@qq.com>

--- src/WatchIO/WatchIO.ino
+++ src/WatchIO/WatchIO.ino
@@ -2,16 +2,20 @@
 #include "src/Adafruit_GFX_Library/Adafruit_GFX.h"    // Core graphics library
 #include "src/Adafruit_ST7735_and_ST7789_Library/Adafruit_ST7735.h" // Hardware-specific library for ST7735
 #include <SPI.h>
+#include <Wire.h>
 
-#define TFT_CS        14 // Hallowing display control pins: chip select
-#define TFT_RST       33 // Display reset
-#define TFT_DC        27 // Display data/command select
-#define TFT_BACKLIGHT  15 // Display backlight pin
+#include "src/RTCLib_by_NeiroN/RTClib.h"
+#include <Adafruit_BMP280.h>
 
-#define SWITCH_UP  12
-#define SWITCH_DOWN  9
-#define SWITCH_PUSH  13
-#define BTN  0
+#define TFT_CS          14 // Hallowing display control pins: chip select
+#define TFT_RST         33 // Display reset
+#define TFT_DC          27 // Display data/command select
+#define TFT_BACKLIGHT   15 // Display backlight pin
+
+#define SWITCH_UP        9
+#define SWITCH_DOWN     12
+#define SWITCH_PUSH     13
+#define BTN              0
 
 int sensorPin = 32;    // select the input pin for the potentiometer
 int sensorValue = 0;  // variable to store the value coming from the sensor
@@ -19,31 +23,52 @@ int sensorValue = 0;  // variable to store the value coming from the sensor
 // For 1.44" and 1.8" TFT with ST7735 (including HalloWing) use:
 Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
 
+Adafruit_BMP280 bmp; // I2C
+PCF8563 rtc;
+
 static boolean sw_up_active = false;
 static boolean sw_down_active = false;
 static boolean sw_push_active = false;
 static boolean btn_active = false;
 
-static int brightness = 240;
+
+const int brightness_max = 255;
+const int brightness_delta = brightness_max / 10;
+
+static int brightness = brightness_max - brightness_delta;
 
 float p = 3.1415926;
 
 void sw_up() {
   sw_up_active = true;
 }
+
 void sw_down() {
   sw_down_active = true;
 }
+
 void sw_push() {
   sw_push_active = true;
 }
+
 void btn() {
   btn_active = true;
 }
 
+bool bmp280_initialized = false;
+
 void setup(void) {
   Serial.begin(115200);
   Serial.print(F("Hello! ST77xx TFT Test"));
+
+  // use this initializer (uncomment) if using a 0.96" 180x60 TFT:
+  tft.initR(INITR_MINI160x80);  // Init ST7735S mini display
+
+  // hack is a need
+  tft.invertDisplay(true);
+
+  // init color before backlight
+  tft.fillScreen(ST77XX_BLUE);
 
   pinMode(SWITCH_UP, INPUT);
   pinMode(SWITCH_DOWN, INPUT);
@@ -54,48 +79,62 @@ void setup(void) {
   attachInterrupt(SWITCH_PUSH, sw_push, FALLING);
   attachInterrupt(BTN, btn, FALLING);
 
+  Wire.begin();
+
+  if (!bmp.begin(BMP280_ADDRESS_ALT)) {
+    Serial.println(F("Could not find a valid BMP280 sensor, check wiring!"));
+    bmp280_initialized = false;
+  } else {
+    bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. */
+                    Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
+                    Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
+                    Adafruit_BMP280::FILTER_X16,      /* Filtering. */
+                    Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
+    bmp280_initialized = true;
+  }
+
+  rtc.begin();
+  if (!rtc.isrunning()) {
+    Serial.println("RTC is NOT running!");
+    // following line sets the RTC to the date & time this sketch was compiled
+    // rtc.adjust(DateTime(__DATE__, __TIME__));
+  }
+
+  Serial.println(F("Initialized"));
+
   // setup pwm channel
   ledcSetup(0, 5000, 8);
   ledcAttachPin(TFT_BACKLIGHT, 0);
   // low is backlight on
   ledcWrite(0, brightness);
 
-  // use this initializer (uncomment) if using a 0.96" 180x60 TFT:
-  tft.initR(INITR_MINI160x80);  // Init ST7735S mini display
-
-  // hack is a need
-  tft.invertDisplay(true);
-
-  Serial.println(F("Initialized"));
-
   uint16_t time = millis();
   // tft.fillScreen(ST77XX_WHITE);
   time = millis() - time;
 
   Serial.println(time, DEC);
-  delay(500);
 
   // rotation will affect on rowstart and colstart
   tft.setRotation(1);
 
-  tft.fillScreen(ST77XX_BLACK);
-  delay(500);
   tft.fillScreen(ST77XX_WHITE);
-  delay(500);
+  delay(100);
+  tft.fillScreen(ST77XX_BLACK);
+  delay(100);
   tft.fillScreen(ST77XX_RED);
-  delay(500);
+  delay(100);
   tft.fillScreen(ST77XX_GREEN);
-  delay(500);
+  delay(100);
   tft.fillScreen(ST77XX_BLUE);
-  delay(500);
+  delay(100);
   tft.fillScreen(ST77XX_CYAN);
-  delay(500);
+  delay(100);
   tft.fillScreen(ST77XX_MAGENTA);
-  delay(500);
+  delay(100);
   tft.fillScreen(ST77XX_YELLOW);
-  delay(500);
+  delay(100);
   tft.fillScreen(ST77XX_ORANGE);
-  delay(500);
+  delay(100);
 
   tft.fillScreen(ST77XX_BLACK);
   // large block of text
@@ -104,42 +143,106 @@ void setup(void) {
 
   // tft print function!
   tftPrintTest();
-  delay(1000);
+  delay(200);
 
   // a single pixel
   tft.drawPixel(tft.width() / 2, tft.height() / 2, ST77XX_GREEN);
-  delay(500);
+  delay(200);
 
   // line draw test
   testlines(ST77XX_YELLOW);
-  delay(500);
+  delay(200);
 
   // optimized lines
   testfastlines(ST77XX_RED, ST77XX_BLUE);
-  delay(500);
+  delay(200);
 
   testdrawrects(ST77XX_GREEN);
-  delay(500);
+  delay(200);
 
   testfillrects(ST77XX_YELLOW, ST77XX_MAGENTA);
-  delay(500);
+  delay(200);
 
   tft.fillScreen(ST77XX_BLACK);
   testfillcircles(10, ST77XX_BLUE);
   testdrawcircles(10, ST77XX_WHITE);
-  delay(500);
+  delay(200);
 
   testroundrects();
-  delay(500);
+  delay(200);
 
   testtriangles();
-  delay(500);
-
-  mediabuttons();
-  delay(500);
+  delay(200);
 
   Serial.println("done");
-  delay(1000);
+}
+
+void i2c_scan() {
+  byte error, address;
+  int nDevices;
+
+  Serial.println("Scanning...");
+
+  nDevices = 0;
+  for (address = 1; address < 127; address++ ) {
+    // The i2c_scanner uses the return value of
+    // the Write.endTransmisstion to see if
+    // a device did acknowledge to the address.
+    Wire.beginTransmission(address);
+    error = Wire.endTransmission();
+
+    if (error == 0) {
+      Serial.print("I2C device found at address 0x");
+      tft.print("Found addr=0x");
+      if (address < 16) {
+        Serial.print("0");
+        tft.println("0");
+      }
+      Serial.print(address, HEX);
+      tft.println(address, HEX);
+      Serial.println("  !");
+      nDevices++;
+    } else if (error == 4) {
+      Serial.print("Unknown error at address 0x");
+      if (address < 16)
+        Serial.print("0");
+      Serial.println(address, HEX);
+    }
+  }
+  if (nDevices == 0) {
+    Serial.println("No I2C devices found\n");
+  }
+  else {
+    Serial.println("done\n");
+  }
+}
+
+void print_bmp280() {
+  if (bmp280_initialized) {
+    float temperature = bmp.readTemperature();
+    tft.print(F("Temperature = "));
+    tft.print(temperature);
+    tft.println(" *C");
+
+    float pressure = bmp.readPressure();
+    tft.print(F("Pressure = "));
+    tft.print(pressure);
+    tft.println(" Pa");
+
+    float altitude = bmp.readAltitude(1013.25);
+    tft.print(F("Approx altitude = "));
+    tft.print(altitude); /* Adjusted to local forecast! */
+    tft.println(" m");
+  }
+}
+
+void print_time() {
+  DateTime now = rtc.now();
+  char buf[100];
+  strncpy(buf, "DD.MM.YYYY  hh:mm:ss\0", 100);
+  now.format(buf);
+  Serial.println(buf);
+  tft.println(buf);
 }
 
 void loop() {
@@ -147,11 +250,13 @@ void loop() {
   tft.fillScreen(ST77XX_YELLOW);
 
   sensorValue = analogRead(sensorPin);
-  double voltage =  sensorValue / 4096.0 * 3.3 * 4.0 / 3.0 * 1.084;
+  double voltage = sensorValue / 4096.0 * 3.3 * 4.0 / 3.0 * 1.113;
 
-  tft.setCursor(10, 10);
+  tft.setCursor(0, 0);
   tft.setTextColor(ST77XX_RED);
   tft.setTextWrap(true);
+
+  print_time();
 
   tft.println(sensorValue);
   tft.print(" batt=");
@@ -159,14 +264,23 @@ void loop() {
   tft.print("V");
   tft.println();
 
+  print_bmp280();
+
   if (btn_active) {
     btn_active = false;
-    tft.println("btn clicked, reboot now!");
-    delay(2000);
-    ESP.restart();
+    esp_sleep_enable_ext0_wakeup((gpio_num_t)BTN, 0); //1 = High, 0 = Low
+    tft.println("btn clicked, sleep now!");
+    delay(5000);
+    tft.sendCommand(ST77XX_SLPIN);
+    Serial.println("sleep tft..");
+    delay(5000);
+    Serial.println("now go to sleep..");
+    esp_deep_sleep_start();
+    Serial.println("this will never be printed.");
+    // ESP.restart();
   } else if (sw_up_active) {
     sw_up_active = false;
-    brightness -= 32;
+    brightness -= brightness_delta;
     if (brightness <= 0) {
       brightness = 0;
     }
@@ -176,16 +290,20 @@ void loop() {
     tft.println(brightness);
   } else if (sw_down_active) {
     sw_down_active = false;
-    brightness += 32;
-    if (brightness >= 255) {
-      brightness = 255;
+    brightness += brightness_delta;
+    if (brightness >= brightness_max) {
+      brightness = brightness_max;
     }
     ledcWrite(0, brightness);
     tft.println("decrease brightness");
     tft.print(" brightness=");
     tft.println(brightness);
+  } else if (sw_push_active) {
+    sw_push_active = false;
+    tft.println("sw push, i2c_scan");
+    i2c_scan();
   }
-  delay(500);
+  delay(900);
 }
 
 void testlines(uint16_t color) {
@@ -354,25 +472,4 @@ void tftPrintTest() {
   tft.print(millis() / 1000);
   tft.setTextColor(ST77XX_WHITE);
   tft.print(" seconds.");
-}
-
-void mediabuttons() {
-  // play
-  tft.fillScreen(ST77XX_BLACK);
-  tft.fillRoundRect(25, 10, 78, 60, 8, ST77XX_WHITE);
-  tft.fillTriangle(42, 20, 42, 60, 90, 40, ST77XX_RED);
-  delay(500);
-  // pause
-  tft.fillRoundRect(25, 90, 78, 60, 8, ST77XX_WHITE);
-  tft.fillRoundRect(39, 98, 20, 45, 5, ST77XX_GREEN);
-  tft.fillRoundRect(69, 98, 20, 45, 5, ST77XX_GREEN);
-  delay(500);
-  // play color
-  tft.fillTriangle(42, 20, 42, 60, 90, 40, ST77XX_BLUE);
-  delay(50);
-  // pause color
-  tft.fillRoundRect(39, 98, 20, 45, 5, ST77XX_RED);
-  tft.fillRoundRect(69, 98, 20, 45, 5, ST77XX_RED);
-  // play color
-  tft.fillTriangle(42, 20, 42, 60, 90, 40, ST77XX_GREEN);
 }
